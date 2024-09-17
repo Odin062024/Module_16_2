f54ad5b4a894be03c3a4ba85b167f371ed6dc312
Wojciech Mazur <odinkodilla@int.pl>
2024-09-17 21:35:22 +0200

    Add more test data

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,11 @@
 from flask import render_template
 from blog import app
+from blog.models import Entry
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    # Pobieranie wszystkich opublikowanych wpisów, posortowanych malejąco według daty publikacji
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc()).all()
+    
+    # Renderowanie szablonu strony głównej i przekazanie wpisów
+    return render_template("homepage.html", all_posts=all_posts)
