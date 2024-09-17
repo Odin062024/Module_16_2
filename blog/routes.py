85c600865ade6b981234573b92ca050d046af42e
Wojciech Mazur <odinkodilla@int.pl>
2024-09-17 22:34:57 +0200

    Add refracte

--- blog/routes.py
+++ blog/routes.py
@@ -1,30 +1,42 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request
 from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm
 
 @app.route("/")
 def index():
-    # Pobieranie wszystkich opublikowanych wpisów, posortowanych malejąco według daty publikacji
-    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc()).all()
-    
-    # Renderowanie szablonu strony głównej i przekazanie wpisów
+    # Pobieranie wszystkich opublikowanych wpisów
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
     return render_template("homepage.html", all_posts=all_posts)
 
-@app.route("/new-post/", methods=["GET", "POST"])
-def create_entry():
-    form = EntryForm()
+# Wspólna funkcja dla tworzenia i edycji wpisów
+def save_entry(entry=None):
+    form = EntryForm(obj=entry)
     errors = None
+    
     if request.method == 'POST':
         if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data,
-                body=form.body.data,
-                is_published=form.is_published.data
-            )
-            db.session.add(entry)
+            if entry is None:  # Tworzenie nowego wpisu
+                entry = Entry()
+                db.session.add(entry)
+            
+            form.populate_obj(entry)
             db.session.commit()
-            return redirect(url_for('index'))
+            return redirect(url_for('homepage'))  # Powrót na stronę główną po zapisie
+        
         else:
             errors = form.errors
+    
     return render_template("entry_form.html", form=form, errors=errors)
+
+# Funkcja obsługująca tworzenie nowego wpisu
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    return save_entry()
+
+# Funkcja obsługująca edycję wpisu
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    return save_entry(entry)
+
